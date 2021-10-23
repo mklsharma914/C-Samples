b15f634978d5589fe866506ac7aa8d19929561e2
kumar46nn <93042003+kumar46nn@users.noreply.github.com>
2021-10-23 19:00:17 +0530

    Update P1_Rec_Area.c

--- P1_Rec_Area.c
+++ P1_Rec_Area.c
@@ -1,15 +1,14 @@
-// Write a C program to calculate area of a rectangle.
-
 #include<stdio.h>
+#include<conio.h>
 
-int main()
+void main()
 { 
-    float l, b, area;
+    float l, b, area;  //l = length, b = breadth
     printf("Enter Length(in cm) : ");
     scanf("%f",&l);
     printf("Enter Breadth(in cm) : ");
     scanf("%f",&b);
     area = l * b;
     printf("Area of Rectangle is : %f sq. cm", area);
-    return 0;
+    getch();
 }
