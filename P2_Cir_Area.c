78057e07d8800ea9b121a1564ffddb42e180309a
kumar46nn <93042003+kumar46nn@users.noreply.github.com>
2021-10-23 19:02:10 +0530

    Update P2_Cir_Area.c

--- P2_Cir_Area.c
+++ P2_Cir_Area.c
@@ -1,9 +1,14 @@
 #include<stdlib.h>
+#include<conio.h>
 
-int main()
+void main()
 {
     float radius_of_circle;
+    
     printf("Enter Radius(in cm) : ");
     scanf("%f",&radius_of_circle);
+    
     printf("Area of circle is : %f", 3.14 * radius_of_circle * radius_of_circle);
+    
+    getch();
 }
